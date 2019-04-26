b3899d2facbf75bc793266c8efe85aa016375ac0
New <new@News-MacBook.local>
2019-04-26 01:49:27 -0700

    a

--- steemconnect/02_steemconnect/index.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa');
-const app = new Koa();
-const serve = require('koa-static');
-app.use(serve('./public'));
-
-app.listen(3000);
-
-console.log('listening on port 3000');
--- steemconnect/02_steemconnect/webpack.config.js
+++
@@ -1,8 +0,0 @@
-var path = require('path');
-module.exports = {
-    entry: './public/app.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-};
